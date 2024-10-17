e0857edb527ba9b22d65c859431ab10d59e33c7d
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-10-17 13:24:38 +0200

    Sync with the latest 19.x branch

--- generate.sh
+++ generate.sh
@@ -8,16 +8,16 @@ if [ ! -d "$LLVM_DIR" ]; then
 fi
 
 pushd "$LLVM_DIR"
+git fetch
 for i in {13..19}; do
     echo "[INFO] Processing LLVM $i.x..."
-    git checkout "release/$i.x"
+    git checkout -f "origin/release/$i.x"
     DEST="$REPO_DIR/$i.x"
     rm -rf $DEST
     mkdir -p $DEST
-    echo "[INFO] Copying files for LLVM $i..."
     cp -r llvm/include/llvm $DEST
     cp -r llvm/include/llvm-c $DEST
-    rm -f $DEST/llvm/module.*
-    find $DEST -name "CMakeLists.txt" -type f -delete
+    find $DEST/llvm -name "module.*" -type f -delete > /dev/null 2>&1
+    find $DEST -name "CMakeLists.txt" -type f -delete > /dev/null 2>&1
 done
 popd
